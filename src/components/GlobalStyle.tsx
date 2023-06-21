65081377561d4e436e4ca0bfe264c912adbbf103
James Muscat <jamesremuscat@gmail.com>
2023-06-21 16:42:18 +0100

    Add styled-components.

---
+++ src/components/GlobalStyle.tsx
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+
+:root {
+  font-family: ${props => props.theme?.font?.family || 'sans-serif'};
+  line-height: 1.5;
+  font-weight: 400;
+
+  color-scheme: dark light;
+  color: ${props => props.theme?.font?.color || 'white'};
+  background-color: ${props => props.theme?.backgroundColor};
+
+  font-synthesis: none;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-text-size-adjust: 100%;
+}
+
+`;
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+export const Theme = {
+  font: {
+    family: '"TT Norms Pro", sans-serif',
+    color: '#FFFFFF'
+  },
+  backgroundColor: '#111420'
+}
